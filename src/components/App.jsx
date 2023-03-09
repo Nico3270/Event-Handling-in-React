1ba89d6633e48f4fcc894e2adeee044c66e95823
Nicolás R <cristianrodriguez3270@hotmail.com>
2023-03-09 18:10:43 -0500

    Cambio color de un boton utilizando hooks, mouseClickOver y mouseClickOut

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
 function App() {
+  const [heading, setHeading] = React.useState("Hello");
+  const [colorBu, setColor] = React.useState({});
+  function changeHeading(){
+    let texto = "Enviado";
+    heading==="Hello"?
+    setHeading(texto)
+    :
+    setHeading("Hello")
+  }
+
+  function changeColor() {
+    setColor({ backgroundColor: "#2f3e46", color: "#cad2c5" });
+  }
+
+  function quitColor() {
+    setColor({ });
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onMouseOver={changeColor} onMouseOut={quitColor} onClick={changeHeading} style={colorBu}>Submit</button>
     </div>
   );
 }
