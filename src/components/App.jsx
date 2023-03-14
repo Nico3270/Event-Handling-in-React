170b78a09156eb2806a03b6540a4c3039967c2e4
Nicolás R <cristianrodriguez3270@hotmail.com>
2023-03-14 12:31:42 -0500

    Agrego el espacio de 'email' al formulario, y lo renderizo en la app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,10 @@ import React from "react";
 function App() {
   const [heading, setHeading] = React.useState("Hello");
   const [colorBu, setColor] = React.useState({});
-  const [fullName, setFullName] = React.useState({
+  const [contact, setContact] = React.useState({
     fName: "",
     lName: "",
+    email: "",
   });
   function changeHeading() {
     let texto = "Hello ";
@@ -21,18 +22,27 @@ function App() {
   }
 
   function handleChange(event) {
-    const { name, value } = event.target;
+    const { name, value, placeholder } = event.target;
     //Se utiliza setFullname y se crea una función flecha que contiene el objeto áctual dentro de preValue
-    setFullName((prevValue) => {
+    setContact((prevValue) => {
       if (name === "fName") {
+        //Se crea un nuevo objeto a partir de preValue y los valores en input
         return {
           fName: value,
           lName: prevValue.lName,
+          email: prevValue.email,
         };
       } else if (name === "lName") {
         return {
           fName: prevValue.fName,
           lName: value,
+          email: prevValue.email,
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
         };
       }
     });
@@ -41,22 +51,31 @@ function App() {
   return (
     <div className="container">
       <h1>
-        {heading} {fullName.fName} {fullName.lName}
+        {heading} {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
+      <br></br>
       <form>
         <input
           onChange={handleChange}
           type="text"
           name="fName"
-          value={fullName.fName}
+          value={contact.fName}
           placeholder="¿Cuál es tu nombre?"
         />
         <input
           onChange={handleChange}
           type="text"
           name="lName"
-          value={fullName.lName}
+          value={contact.lName}
           placeholder="¿Cuál es tu apellido?"
+        />
+        <input
+          onChange={handleChange}
+          type="email"
+          name="email"
+          value={contact.email}
+          placeholder="¿Cuál es tu email?"
         />
         <button
           onMouseOver={changeColor}
